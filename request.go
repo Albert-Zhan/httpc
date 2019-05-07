ed2c8419f652f66b7c5950c015429dc3fe8bb052
AlbertZhan <2654709623@qq.com>
2019-05-07 17:10:34 +0800

    Fix open debug mode cookie does not display problem
    Fix open debug mode cookie does not display problem

--- request.go
+++ request.go
@@ -21,6 +21,7 @@ type fileData struct {
 
 type Request struct {
 	httpc *HttpClient
+	request *http.Request
 	response *http.Response
 	method string
 	url string
@@ -65,23 +66,27 @@ func (this *Request) SetCookies(cookies *[]*http.Cookie) *Request  {
 	return this
 }
 
-func (this *Request) SetData(name,value string) *Request {
-	this.data.Set(name,value)
-	return this
-}
-
 func (this *Request) SetDebug(d bool) *Request {
 	this.debug=d
 	return this
 }
 
+func (this *Request) SetData(name,value string) *Request {
+	this.data.Set(name,value)
+	return this
+}
+
 func (this *Request) SetFileData(name,value string,isFile bool) *Request  {
 	if isFile==true{
-		fileWriter,_:=this.fileData.bodyWrite.CreateFormFile(name,filepath.Base(value))
+		fd,err:=os.Open(value)
 
-		fd,_:=os.Open(value)
+		if err!=nil {
+			this.err=err
+			return this
+		}
 		defer fd.Close()
 
+		fileWriter,_:=this.fileData.bodyWrite.CreateFormFile(name,filepath.Base(value))
 		_,_=io.Copy(fileWriter,fd)
 	}else{
 		_ = this.fileData.bodyWrite.WriteField(name, value)
@@ -91,81 +96,85 @@ func (this *Request) SetFileData(name,value string,isFile bool) *Request  {
 }
 
 func (this *Request) Send() *Request {
-
 	_ = this.fileData.bodyWrite.Close()
-
-	request,err:=http.NewRequest(this.method,this.url,strings.NewReader(this.data.Encode()))
-	defer this.log(request)
+	var err error
+	this.request,err=http.NewRequest(this.method,this.url,strings.NewReader(this.data.Encode()))
+	defer this.log(false)
 	if err!=nil {
 		this.err=err
 		return this
 	}
 
 	if this.method=="POST" {
-		request.Header.Set("Content-Type","application/x-www-form-urlencoded")
+		this.request.Header.Set("Content-Type","application/x-www-form-urlencoded")
 	}
 	for k,v:=range this.header {
-		request.Header.Set(k,v)
+		this.request.Header.Set(k,v)
 	}
 
 	for _,v:= range *this.cookies {
-		request.AddCookie(v)
+		this.request.AddCookie(v)
 	}
 	
-	response,err:=this.httpc.client.Do(request)
+	response,err:=this.httpc.client.Do(this.request)
 	if err!=nil {
 		this.err=err
 		return this
 	}
-	this.response=response
 
+	this.response=response
 	return this
 }
 
 func (this *Request) SendFile() *Request {
-
 	contentType:=this.fileData.bodyWrite.FormDataContentType()
 	_ = this.fileData.bodyWrite.Close()
+	var err error
 
-	request,err:=http.NewRequest(this.method,this.url,ioutil.NopCloser(this.fileData.bodyBuf))
-	defer this.log(request)
+	this.request,err=http.NewRequest(this.method,this.url,ioutil.NopCloser(this.fileData.bodyBuf))
+	defer this.log(true)
 	if err!=nil {
 		this.err=err
 		return this
 	}
 
-	request.Header.Set("Content-Type",contentType)
-	request.Cookies()
+	this.request.Header.Set("Content-Type",contentType)
 	for k,v:=range this.header {
-		request.Header.Set(k,v)
+		this.request.Header.Set(k,v)
 	}
 
 	for _,v:= range *this.cookies {
-		request.AddCookie(v)
+		this.request.AddCookie(v)
 	}
 
-	response,err:=this.httpc.client.Do(request)
+	response,err:=this.httpc.client.Do(this.request)
 	if err!=nil {
 		this.err=err
 		return this
 	}
+
 	this.response=response
-
 	return this
-
 }
 
-func (this *Request) log(req *http.Request) {
+func (this *Request) log(isFile bool) {
 	if this.debug==true {
-		fmt.Printf("[HttpRequest Debug]\n")
+		var data =make(map[string][]string)
+		if isFile {
+
+		}else{
+			for k,v:= range this.data {
+				data[k]=v
+			}
+		}
+		fmt.Printf("[httpc Debug]\n")
 		fmt.Printf("-------------------------------------------------------------------\n")
-		fmt.Printf("Request: %s %s\nHeader: %v\nBody: %s\n",this.method,this.url,req.Header,this.data)
-		fmt.Printf("-------------------------------------------------------------------\n\n")
+		fmt.Printf("Request: %s %s\nHeader: %v\nCookies: %v\nBody: %v\n",this.method,this.url,this.request.Header,this.request.Cookies(),data)
+		fmt.Printf("-------------------------------------------------------------------\n")
 	}
 }
 
 func (this *Request) End() (*http.Response,string,error) {
-
 	if this.err!=nil {
 		return nil,"",errors.New(this.err.Error())
 	}
@@ -177,7 +186,6 @@ func (this *Request) End() (*http.Response,string,error) {
 }
 
 func (this *Request) EndByte() (*http.Response,[]byte,error) {
-
 	if this.err!=nil {
 		return nil,[]byte(""),errors.New(this.err.Error())
 	}
@@ -186,4 +194,18 @@ func (this *Request) EndByte() (*http.Response,[]byte,error) {
 
 	return this.response,bodyByte,nil
 
+}
+
+func (this *Request) EndFile(saveFile string) (*http.Response,error)  {
+	if this.err!=nil {
+		return nil,errors.New(this.err.Error())
+	}
+
+	bodyByte,_:=ioutil.ReadAll(this.response.Body)
+	err:= ioutil.WriteFile(saveFile, bodyByte, 0777)
+	if err!=nil {
+		return nil,errors.New(err.Error())
+	}
+	
+	return this.response,nil
 }
