916d81edf438cb42bede10e11a617bbfb4be6a1b
Albert <17407006+Albert-Zhan@users.noreply.github.com>
2025-11-14 16:24:42 +0800

    Update request.go

--- request.go
+++ request.go
@@ -16,6 +16,7 @@ import (
 	"strings"
 )
 
+// Request 封装了 HTTP 请求构建和发送的逻辑
 type Request struct {
 	httpc    *HttpClient
 	request  *http.Request
@@ -30,6 +31,7 @@ type Request struct {
 	err      error
 }
 
+// NewRequest 创建一个新的 Request 对象，默认使用 GET 方法
 func NewRequest(client *HttpClient) *Request {
 	return &Request{
 		httpc:   client,
@@ -42,31 +44,37 @@ func NewRequest(client *HttpClient) *Request {
 	}
 }
 
+// SetClient 替换请求使用的 HttpClient
 func (this *Request) SetClient(client *HttpClient) *Request {
 	this.httpc = client
 	return this
 }
 
+// SetMethod 设置 HTTP 请求方法，自动转为大写
 func (this *Request) SetMethod(name string) *Request {
 	this.method = strings.ToUpper(name)
 	return this
 }
 
+// SetUrl 设置请求的URL地址
 func (this *Request) SetUrl(url string) *Request {
 	this.url = url
 	return this
 }
 
+// SetParam 添加 URL 查询参数，支持多次链式调用
 func (this *Request) SetParam(name, value string) *Request {
 	this.param.Add(name, value)
 	return this
 }
 
+// SetHeader 添加或修改请求头，支持多次链式调用
 func (this *Request) SetHeader(name, value string) *Request {
 	this.header[name] = value
 	return this
 }
 
+// SetBasicAuth 设置 HTTP Basic Auth 认证
 func (this *Request) SetBasicAuth(username, password string) *Request {
 	auth := username + ":" + password
 	value := "Basic " + base64.StdEncoding.EncodeToString([]byte(auth))
@@ -74,21 +82,27 @@ func (this *Request) SetBasicAuth(username, password string) *Request {
 	return this
 }
 
+// SetCookies 设置请求的 Cookie 列表
 func (this *Request) SetCookies(cookies *[]*http.Cookie) *Request {
 	this.cookies = cookies
 	return this
 }
 
+// SetDebug 设置是否开启调试模式
+// 开启后会打印请求方法、URL、头信息、Cookie 和 Body
 func (this *Request) SetDebug(d bool) *Request {
 	this.debug = d
 	return this
 }
 
+// SetBody 设置请求体，实现 body.Body 接口
 func (this *Request) SetBody(body body.Body) *Request {
 	this.data = body
 	return this
 }
 
+// Send 构建并发送 HTTP 请求
+// 可选传入 context，用于控制请求超时或取消
 func (this *Request) Send(ctxs ...context.Context) *Request {
 	param := this.param.Encode()
 	if param != "" {
@@ -132,14 +146,17 @@ func (this *Request) Send(ctxs ...context.Context) *Request {
 	return this
 }
 
+// GetResponse 返回 HTTP 响应对象
 func (this *Request) GetResponse() *http.Response {
 	return this.response
 }
 
+// GetError 返回请求过程中发生的错误
 func (this *Request) GetError() error {
 	return this.err
 }
 
+// log 调试模式下打印请求详细信息
 func (this *Request) log() {
 	if this.debug == true {
 		data := ""
@@ -154,6 +171,7 @@ func (this *Request) log() {
 	}
 }
 
+// End 执行请求并返回响应对象、响应内容字符串以及错误
 func (this *Request) End() (*http.Response, string, error) {
 	resp, bodyByte, err := this.EndByte()
 	if err != nil {
@@ -162,6 +180,8 @@ func (this *Request) End() (*http.Response, string, error) {
 	return resp, string(bodyByte), nil
 }
 
+// EndByte 执行请求并返回响应对象、响应内容字节数组以及错误
+// 自动处理 gzip 压缩
 func (this *Request) EndByte() (*http.Response, []byte, error) {
 	if this.err != nil {
 		return nil, nil, this.err
@@ -200,6 +220,8 @@ func (this *Request) EndByte() (*http.Response, []byte, error) {
 	return this.response, bodyByte, nil
 }
 
+// EndFile 将响应体保存为文件
+// savePath 为目录路径，saveFileName 可为空，自动根据 URL 获取文件名
 func (this *Request) EndFile(savePath, saveFileName string) (*http.Response, error) {
 	if this.err != nil {
 		return nil, this.err
